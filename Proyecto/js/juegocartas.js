3f4f1dcd071f857fe8c583b9e2f569729d06dc06
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2016-03-19 23:56:40 +0100

    En proceso de adaptación del código al uso de POO

--- Proyecto/js/juegocartas.js
+++ Proyecto/js/juegocartas.js
@@ -1,83 +1,202 @@
 "use strict";
 
 /**
- * Función para obtener un rango de valores. Similar a range de Python.
- *
- * @param ini Límite inicial del rango. > 0
- * @param fin Límite final (no incluído entre los valores). Opcional y > ini.
- * @param paso Paso a incrementar cada valor en el rango devuelto.
- * @return Array con valores desde 0 a ini-1 (en incrementos de paso) si sólo 
- * se pasa ini; o array con valores entre ini y fin-1 (en incrementos de paso) 
- * si se pasa también fin. Null si hay algún error.
+ * Mejoras:
  */
-function range( ini, fin, paso=1)
-{
-    ini = parseInt( ini);
-    paso = parseInt( paso);
-    if (isNaN( ini) || isNaN( paso) || paso === 0) return null;
-
-    if (fin === void 0)
-    {        
-        if (paso < 0 || ini < 0) return null;
-        return Array(Math.ceil(ini/paso)).map( (v, i)=>i*paso);
-    }
-
-    let signo = Math.sign( paso);
-    fin = parseInt( fin);
-    if (isNaN( fin) || fin <= ini*signo) return null;
-
-    return Array(Math.ceil( (fin-ini)/paso)).map( (v, i) => ini + i*paso);
-} 
 
 
-const MAX_DIM = 8;
-var dimensiones = range( 2, MAX_DIM+1, 2);
-
-/**
- * Limpiar el tablero de cartas.
- */
-function limpiarTablero() 
-{
-
-}
+import {range, ObjetoDOM, defPrivados} from 'utilidades';
 
 
 /**
- * Inicializar el tablero de cartas.
- *
- * @param dimension Dimensión del tablero.
- * @return true si se inicializa correctamente; false si hay algún error.
+ * Clase Tablero donde están todas las cartas.
  */
-function crearTablero( dimension)
+var Tablero = (function()
 {
-    let tablero = document.getElementById( "tablero");
-    if (tablero === null) return false;
-    let totalCartas = Math.pow( dimension, 2);
-    let valores = range( 1, totalCartas/2 + 1);
-    valores = valores.concat( valores);
+    var priv = defPrivados();
 
-    for (let i = 1; i <= totalCartas; ++i)
+    /**
+     * Clase Carta
+     */
+    class Carta extends ObjetoDOM
     {
-        let carta = document.createElement( "span");
-        if (carta === null) return false;
-        carta.class = "carta";
-        carta.id = "carta"+i;
-        carta.name = "carta"+i;
-        let k = parseInt( Math.random()*valores.length);
-        carta.value = valores[k];
-        valores.splice( k); 
-        tablero.appendChild( carta);
+        /**
+         * Contructor de carta.
+         *
+         * @param id Id a usar como objeto DOM.
+         * @param name Name a usar como objeto DOM.
+         * @param obj Objeto DOM relacionado con la carta. Por defecto span.
+         */
+        constructor( id, name, obj = document.createElement( "span"))
+        {
+            super( obj);
+            this.DOM.classList.add( "carta", "abajo");
+            this.DOM.id = id;
+            this.DOM.name = name;
+            this.DOM.style.display = "none";
+            this.DOM.onclick = Carta.prototype.pulsar;
+            let thisPrv = priv( this);
+            thisPrv._bocaAbajo = true;
+            thisPrv._visible = false;
+            thisPrv._valor = null;  
+        }
+
+        /**
+         * Propiedad para saber si la carta está boca abajo
+         *
+         * @return True si la carta está boca abajo. False si está boca arriba.
+         */
+        get estaHaciaAbajo()
+        {
+            return priv( this)._bocaAbajo;
+        }
+
+        /**
+         * Propiedad para saber si la carta debe estar visible en el DOM.
+         *
+         * @return True/False si la carta debe estar visible o no en el DOM.
+         */
+        get esVisibleDOM()
+        {
+            return priv( this)._visible;
+        }
+
+        /**
+         * Cambiar la visibilidad del objeto DOM asociado con la carta.
+         *
+         * @param flag True/False si se desea hacer o no visible.
+         */
+        set visibleDOM( flag)
+        {
+            let thisPrv = priv( this);
+            thisPrv._visible = Boolean( flag);
+            this.DOM.style.display = thisPrv._visible ? "inline" : "none";
+        }
+
+        /**
+         * Obtener el valor de la carta.
+         *
+         * @return Valor de la carta.
+         */
+        get valor()
+        {
+            return priv( this)._valor;
+        }
+
+
+        /**
+         * Dar la vuelta a la carta.
+         */
+        voltear()
+        {
+            let thisPrv = priv( this);
+            thisPrv._bocaAbajo = !thisPrv._bocaAbajo;
+        }
+
+
+        /**
+         */
+        pulsar()
     }
 
-    return true;
+
+    class _Tablero extends ObjetoDOM
+    {    
+        static get MAX_DIM() { return 8; }
+
+        /**
+         * Constructor.
+         *
+         * @param obj Objeto DOM asociado con el tablero.
+         * @param dimension Dimensión máxima que puede tener el tablero (par).
+         */
+        constructor( obj, dimension)
+        {
+            if (dimension % 2 !== 0)
+                throw new PersonalException( "La dimensión máxima no es par");
+
+            super( obj);
+            let thisPrv = priv( this);
+            thisPrv._maxDimension = dimension;
+            thisPrv._cartas = new Array( Math.pow( dimension, 2));
+
+            for (let i in thisPrv._cartas)
+            {   
+                let id = "carta" + (i+1);
+                thisPrv._cartas[i] = new Carta( id, id);
+                this.DOM.appendChild( thisPrv._cartas[i].DOM);
+            }
+        }
+
+
+        /**
+         * Inicializa el tablero de cartas para empezar una nueva partida.
+         *
+         * @param dimension Dimensión del tablero para la partida a iniciar.
+         */
+        inicializaTablero( dimension)
+        {
+            if (dimension % 2 !== 0)
+                throw new PersonalException( "La dimensión no es par", -2);
+            if (dimension > thisPrv._maxDimension)
+                throw new PersonalException( 
+                        "Dimensión de la partida mayor al máximo permitido", -3);
+
+            let thisPrv = priv( this);
+
+            let totalCartas = Math.pow( dimension, 2);
+            let valores = range( 1, totalCartas/2 + 1);
+            valores = valores.concat( valores);
+
+            let i = 0;
+            for (let carta of thisPrv._cartas)
+            {
+                carta.classList.add( "abajo");
+                carta.classList.remove( "arriba");
+                if (i++ >= totalCartas)
+                {
+                    carta.style.display = "none";
+                    continue;
+                }
+
+                let k = parseInt( Math.random()*valores.length);
+                carta.value = valores[k];
+                carta.style.display = "inline";
+                valores.splice( k); 
+           }
+
+            tablero.style.width = Math.ceil( dimension * 100/MAX_DIM)+"%";
+        }      
+
+
+        /**
+         * Destruir el tablero de cartas.
+         */
+        destruir() 
+        {
+            this.DOM.textContent = "";
+            delete priv( this)._cartas;
+        }
+    }
+
+    return _Tablero;
+})();
+
+
+class Partida 
+{
 }
 
 
+
+
+
+
 /**
- * Al cargarse la página inicialmente, se crean los marcadores donde se van a 
- * guardar las estadísticas y también se crean las opciones de selección.
+ * Inicializa las opciones de juego y las tablas de puntuación.
  */
-window.load = function( evento) {
+function inicializarPantallaDeJuego()
+{
     let tabla = document.querySelector("#resultados > table");
     let selector = document.getElementById( "dimension");
     for (let i of dimensiones)
@@ -96,48 +215,101 @@ window.load = function( evento) {
        opcion.value = i;
        selector.appendChild( opcion);
     }
+}
 
-};
+
+var botonEmpezar,
+    botonReiniciar,
+    botonParar;
+    jugando,
+    tablero, 
+    textIntentos,
+    intentosPartida,
+    intentosRestantes,
+    valorIntentosRestantes;
+    dimension,
+    valorDimension,
+    dimensiones;
+
+const MAX_DIM = 8; // Tiene que ser un valor par.
 
 
 /**
  * Cuando se empieza una partida con la configuración elegida.
  */
-var botonEmpezar = document.getElementById( "empezar");
-botonEmpezar.onclick = function( evento) 
+function pulsarBotonEmpezar( evento) 
 {
-    let dimension = parseInt( document.getElementById( "dimension").value);
-    if (isNaN( dimension.value) || dimension < 2 || dimension > MAX_DIM)
+    valorDimension = parseInt( dimension.value);
+    if (isNaN( valorDimension) || valorDimension < 2 || 
+        valorDimension > MAX_DIM)
     {
         alert( "Valor incorrecto de dimensión");
         return;
     }
 
-    let intentos = parseInt( document.getElementById( "intentos").value);    
-    if (isNan( intentos.value) || intentos < 1)
+    valorIntentos = parseInt( intentos.value);    
+    if (isNan( valorIntentos) || valorIntentos < 1)
     {
         alert( "Valor incorrecto de número de intentos");
         return;
     }
 
-    limpiarTablero();
-    if (!crearTablero( dimension))
-    {
-        alert( "Error al inicializar el tablero");
-        return;
-    }  
+    inicializaTablero( valorDimension);
+
+    botonEmpezar.disabled = true;
+    botonParar.disabled = false;
+    jugando = true;
+    valorIntentosRestantes = valorIntentos;
+    intentosRestantes.textContent = valorIntentosRestantes;
 };
 
 
-var botonReiniciar = document.getElementById( "reiniciar");
-botonReiniciar.onclick = function( evento) 
+/**
+ * Cuando se reinician los datos de los resultados.
+ */
+function pulsarBotonReiniciar( evento) 
 {
     if (!confirm( "¿Deseas borrar todos los datos de resultados?")) return;
     
     // Se limpian las estadísticas
     [].forEach.call( document.querySelectorAll( "#resultados td"), 
                      x => x.textContent = 0)
-
-    limpiarTablero();
-
 }
+
+
+/**
+ * Cuando se para una partida empezada.
+ */
+function pulsarBotonParar( evento)
+{
+    botonEmpezar.disabled = false;
+    botonParar.disabled = true;
+    jugando = false;
+}
+
+
+/**
+ * Al cargarse la página inicialmente, se crean los marcadores donde se van a 
+ * guardar las estadísticas y también se crean las opciones de selección.
+ */
+window.load = function( evento) 
+{
+    botonEmpezar = document.getElementById( "empezar");
+    botonEmpezar.onclick = pulsarBotonEmpezar;
+    botonReiniciar = document.getElementById( "reiniciar");
+    botonReiniciar.onclick = pulsarBotonReiniciar;
+    botonParar = document.getElementById( "parar");
+    botonParar.onclick = pulsarBotonParar;
+    dimension = document.getElementById( "dimension");   
+    intentos = document.getElementById( "intentos");
+    intentosRestantes = document.getElementById( "intentos-restantes");   
+    tablero = document.getElementById( "tablero");
+    jugando = false;
+    dimensiones = range( 2, MAX_DIM+1, 2);
+ 
+    inicializarPantallaDeJuego(); 
+    crearTablero();
+};
+
+
+
---
+++ Proyecto/utilidades.js
@@ -0,0 +1,128 @@
+/**
+ * Herramientas con utilidades para otros programas.
+ */
+
+
+/**
+ * Función para obtener un rango de valores. Similar a range de Python.
+ *
+ * @param ini Límite inicial del rango. > 0
+ * @param fin Límite final (no incluído entre los valores). Opcional y > ini.
+ * @param paso Paso a incrementar cada valor en el rango devuelto.
+ * @return Array con valores desde 0 a ini-1 (en incrementos de paso) si sólo 
+ * se pasa ini; o array con valores entre ini y fin-1 (en incrementos de paso) 
+ * si se pasa también fin. Null si hay algún error.
+ */
+export range( ini, fin, paso=1)
+{
+    ini = parseInt( ini);
+    paso = parseInt( paso);
+    if (isNaN( ini) || isNaN( paso) || paso === 0) return null;
+
+    if (fin === void 0)
+    {        
+        if (paso < 0 || ini < 0) return null;
+        return Array(Math.ceil(ini/paso)).map( (v, i)=>i*paso);
+    }
+
+    let signo = Math.sign( paso);
+    fin = parseInt( fin);
+    if (isNaN( fin) || fin <= ini*signo) return null;
+
+    return Array(Math.ceil( (fin-ini)/paso)).map( (v, i) => ini + i*paso);
+} 
+
+
+/**
+ * Función para definir una zona de memoria privada para cada cada uno de los
+ * objetos instanciados de una clase. En dicha zona se podrán guardar miembros
+ * privados de cada objeto.
+ *
+ * @return Función function( obj) que recibe un objeto y devuelve la zona de 
+ * memoria privada asignada para ese objeto instanciado.
+ */
+export function defPrivados()
+{
+    var _objetosPrivados = new WeakMap();
+
+    return function( obj)
+    {
+        if (!_objetosPrivados.has( obj))
+            _objetosPrivados.set( obj, {});
+        return _objetosPrivados.get( obj);
+    };
+}
+
+
+/**
+ * Excepción personalizada.
+ */
+export var PersonalException = (function()
+{
+    var priv = defPrivados();
+
+    class _PersonalException extends Error
+    {
+        constructor( mensaje, codigo = -1)
+        {
+            let thisPrv = priv( this);
+            thisPrv._mensaje = String( mensaje);
+            thisPrv._codigo = Number( codigo);
+        }
+
+        toString()
+        {
+            let thisPrv = priv( this);
+            return thisPrv._codigo + " " + thisPrv._mensaje
+        }
+    }
+
+    return _PersonalException;
+})();
+
+
+/**
+ * Clase para envolver un objeto DOM y darle funcionalidad.
+ */
+export var ObjetoDOM = (function()        
+{
+    var priv = defPrivados();
+
+    class _ObjetoDOM
+    {
+        /**
+         * Constructor.
+         *
+         * @param obj Objeto DOM a envolver.
+         */
+        constructor( obj)
+        {
+            if (!(obj && 'nodeType' in obj))
+                throw new PersonalException( "El objeto no es de tipo DOM");
+
+            let thisPriv = priv( this);
+            thisPriv._DOM = obj;
+        }
+
+        /**
+         * Atributo setter.
+         */
+        set DOM( obj)
+        {
+            priv( this)._DOM = obj;
+        }
+
+        /**
+         * Atributo getter.
+         */
+        get DOM()
+        {
+            return priv( this)._DOM;
+        }
+    }
+
+    return _ObjetoDOM;
+})();
+
+
+
